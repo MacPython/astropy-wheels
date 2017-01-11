a669010bdbc6429cf4cd3f2943c753d2a0fb0519
Tom Kooij <tomkooij@tomkooij.nl>
2017-01-11 14:13:48 +0100

    Force wheel for matplotlib
    Installing matplotlib from sdist fails, so force installation from wheel.
    This prevents pip selecting an sdist from pypi if the sdist version is higher
    than the pre-built wheel in the wheelhouse.

--- config.sh
+++ config.sh
@@ -14,7 +14,7 @@ function pip_opts {
     else
         local suffix=manylinux
     fi
-    echo "--find-links https://nipy.bic.berkeley.edu/$suffix"
+    echo "--only-binary matplotlib --find-links https://nipy.bic.berkeley.edu/$suffix"
 }
 
 function run_tests {
