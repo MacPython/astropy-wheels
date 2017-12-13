1727b087e7d364e8f92e1b32ba78439fd701e002
Brigitta Sipocz <bsipocz@gmail.com>
2017-12-13 19:46:46 +0000

    Try building wheels for v2.0.3, removing patch

--- config.sh
+++ config.sh
@@ -21,10 +21,6 @@ function run_tests {
     # Runs tests on installed distribution from an empty directory
     ASTROPY_INSTALL_DIR=$(dirname $(python -c 'import astropy; print(astropy.__file__)'))
 
-    # Patch the h5py test file to fix test for 2.7.1 error message change
-    local patch_file=$(abspath ../patches/h5py_2.7.1.patch)
-    (cd $ASTROPY_INSTALL_DIR && patch -p0 < $patch_file)
-
     # Runs tests on installed distribution from an empty directory
     python --version
     python -c "import sys; import astropy; sys.exit(astropy.test(remote_data='none'))"
