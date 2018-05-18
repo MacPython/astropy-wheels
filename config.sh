1b28df12d987531fbdaaef1f3338f5a7675799a7
Matthew Brett <matthew.brett@gmail.com>
2018-05-18 18:35:49 +0100

    Move MANYLINUX into config.sh

--- config.sh
+++ config.sh
@@ -1,6 +1,9 @@
 # Define custom utilities
 # Test for OSX with [ -n "$IS_OSX" ]
 
+# Get extra wheels from Rackspace container
+MANYLINUX_URL=https://5cf40426d9f06eb7461d-6fe47d9331aba7cd62fc36c7196769e4.ssl.cf2.rackcdn.com
+
 function pre_build {
     # Any stuff that you need to do before you start building the wheels
     # Runs in the root directory of this repository.
