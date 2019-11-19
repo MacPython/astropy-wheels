0c53d49e37ec660641dfbb72b5f92e4b206a5288
Thomas Robitaille <thomas.robitaille@gmail.com>
2019-11-19 11:38:11 +0000

    Patch the test_system_file_never_expired test

--- config.sh
+++ config.sh
@@ -17,6 +17,11 @@ function run_tests {
     # Runs tests on installed distribution from an empty directory
     ASTROPY_INSTALL_DIR=$(dirname $(python -c 'import astropy; print(astropy.__file__)'))
 
+    # Patch the test_system_file_never_expired test which is affected by
+    # https://github.com/astropy/astropy/issues/9497
+    local patch_file=$(abspath ../patches/test_system_file_never_expired.patch)
+    (cd $ASTROPY_INSTALL_DIR && patch -p0 < $patch_file)
+
     # Runs tests on installed distribution from an empty directory
     python --version
     python -c "import sys; import astropy; sys.exit(astropy.test(remote_data='none'))"
