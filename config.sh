079161d8164fbf08e8f8441734d3b57201d022b3
Brigitta Sipocz <bsipocz@gmail.com>
2019-11-18 19:09:10 -0800

    Reverting patch

--- config.sh
+++ config.sh
@@ -17,12 +17,6 @@ function run_tests {
     # Runs tests on installed distribution from an empty directory
     ASTROPY_INSTALL_DIR=$(dirname $(python -c 'import astropy; print(astropy.__file__)'))
 
-    # Patch the test_roundtrip_sky_rotation test which is affected by angle
-    # wrapping on 32-bit platforms. This should be fixed in astropy v4.0 with
-    # https://github.com/astropy/astropy/pull/9496
-    local patch_file=$(abspath ../patches/test_roundtrip_sky_rotation.patch)
-    (cd $ASTROPY_INSTALL_DIR && patch -p0 < $patch_file)
-
     # Runs tests on installed distribution from an empty directory
     python --version
     python -c "import sys; import astropy; sys.exit(astropy.test(remote_data='none'))"
